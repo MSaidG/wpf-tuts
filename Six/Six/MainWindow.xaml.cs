d6bb29ba82cc128609a3642d91cc44ebf1b2c6a9
MSaidG <msgulberk@gmail.com>
2024-08-31 23:34:56 +0300

    Add message box and file dialog tutorial

---
+++ Six/Six/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace Six
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBoxResult result = MessageBox.Show("Do you agree?", "Agreement",
+            //     MessageBoxButton.YesNo, MessageBoxImage.Question);
+            // 
+            // if (result == MessageBoxResult.Yes)
+            // {
+            //     tbInfo.Text = "Agreed";
+            // }
+            // else
+            // {
+            //     tbInfo.Text = "Not Agreed";
+            // }
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "C# Source Files | *.cs";
+            fileDialog.InitialDirectory = "C:\\";
+            fileDialog.Title = "Select cs files";
+            fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+            if (success == true)
+            {
+                string[] paths = fileDialog.FileNames;
+                string[] fileNames = fileDialog.SafeFileNames;
+
+                //tbInfo.Text = fileName;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
