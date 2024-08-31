953b62f4d66074fb2b14b02b034c98b711a6fd50
MSaidG <msgulberk@gmail.com>
2024-08-31 18:56:45 +0300

    Initial commit

---
+++ One/One/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace One
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+
+            }
+
+            running = !running;
+        }
+    }
+}
