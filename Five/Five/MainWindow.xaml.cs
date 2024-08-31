a787c129f5aed4c0a2540f1b9bc589a031e95955
MSaidG <msgulberk@gmail.com>
2024-08-31 23:11:59 +0300

    Add data binding and notification tutorial

--- Five/Five/MainWindow.xaml.cs
+++ Five/Five/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +16,34 @@ namespace Five
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private string boundText;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            { 
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
